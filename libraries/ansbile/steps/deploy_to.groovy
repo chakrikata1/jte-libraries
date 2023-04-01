0a3022fb18460d78e156948b5e961a4e423b4c46
chakri <chakrikata@gmail.com>
2023-04-01 10:44:56 +0530

    ansible library with env parameters

---
+++ libraries/ansbile/steps/deploy_to.groovy
@@ -0,0 +1,10 @@
+void call(){
+
+    stage("Deploy to: ${app_env.long_name}")
+    println("performing a deploying via ansible")
+    app_env.ip_addresses.each{
+        ip ->
+            println "deploying to ${ip}"
+    }
+
+}
