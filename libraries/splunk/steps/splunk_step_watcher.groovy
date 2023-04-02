eabaaa2a9a73747f5d577e1f52ca0376cfd79c78
chakri <chakrikata@gmail.com>
2023-04-02 09:29:46 +0530

    condition for afterstep

--- libraries/splunk/steps/splunk_step_watcher.groovy
+++ libraries/splunk/steps/splunk_step_watcher.groovy
@@ -3,7 +3,7 @@ void before(){
     println "Splunk: running before the ${hookContext.library} library's ${hookContext.step} step"
 }
 
-@AfterStep
+@AfterStep({ hookContext.step.equals("build") })
 void after(){
     println "Splunk: running after the ${hookContext.library} library's ${hookContext.step} step"
 }
